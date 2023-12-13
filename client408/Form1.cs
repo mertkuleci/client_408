68bded53198e3e6502c15b25eb0f8951aa38d7c8
Hasan Firat <hyilmaz@sabanciuniv.edu>
2023-12-13 17:03:41 +0300

    smt

--- client408/Form1.cs
+++ client408/Form1.cs
@@ -259,6 +259,7 @@ private void ListenForClients()
             {
                 richTextBox6.AppendText("Invalid port number.\n");
                 //Some redundant comment to test github
+                // Mert Kuleci büyüktür
             }
         }
     }
