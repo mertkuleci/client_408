ef90213cf693e1f9a70a3f821bddf55c737835f8
Hasan Firat <hyilmaz@sabanciuniv.edu>
2023-12-13 23:21:38 +0300

    Updated variable names & small changes in the structure of the template.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Server_Application_CS408
+{
+    partial class Server_Application
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox_IP = new TextBox();
+            label_IP = new Label();
+            label_Port = new Label();
+            textBox_Port = new TextBox();
+            button_ServerStart = new Button();
+            label_IF100 = new Label();
+            richTextBox_IF100 = new RichTextBox();
+            richTextBox_SPS101 = new RichTextBox();
+            label_SPS101 = new Label();
+            richTextBox_AllChannels = new RichTextBox();
+            label_All = new Label();
+            richTextBox_Actions = new RichTextBox();
+            label_Actions = new Label();
+            label_ConnectedUsers = new Label();
+            SuspendLayout();
+            // 
+            // textBox_IP
+            // 
+            textBox_IP.Location = new Point(198, 68);
+            textBox_IP.Name = "textBox_IP";
+            textBox_IP.Size = new Size(173, 31);
+            textBox_IP.TabIndex = 0;
+            // 
+            // label_IP
+            // 
+            label_IP.AutoSize = true;
+            label_IP.Location = new Point(78, 74);
+            label_IP.Name = "label_IP";
+            label_IP.Size = new Size(31, 25);
+            label_IP.TabIndex = 1;
+            label_IP.Text = "IP:";
+            // 
+            // label_Port
+            // 
+            label_Port.AutoSize = true;
+            label_Port.Location = new Point(37, 128);
+            label_Port.Name = "label_Port";
+            label_Port.Size = new Size(118, 25);
+            label_Port.TabIndex = 3;
+            label_Port.Text = "Port Number:";
+            // 
+            // textBox_Port
+            // 
+            textBox_Port.Location = new Point(198, 122);
+            textBox_Port.Name = "textBox_Port";
+            textBox_Port.Size = new Size(173, 31);
+            textBox_Port.TabIndex = 4;
+            // 
+            // button_ServerStart
+            // 
+            button_ServerStart.Location = new Point(433, 81);
+            button_ServerStart.Name = "button_ServerStart";
+            button_ServerStart.Size = new Size(174, 55);
+            button_ServerStart.TabIndex = 5;
+            button_ServerStart.Text = "Start To Server";
+            button_ServerStart.UseVisualStyleBackColor = true;
+            button_ServerStart.Click += button_ServerStart_Click;
+            // 
+            // label_IF100
+            // 
+            label_IF100.AutoSize = true;
+            label_IF100.Location = new Point(126, 205);
+            label_IF100.Name = "label_IF100";
+            label_IF100.Size = new Size(56, 25);
+            label_IF100.TabIndex = 6;
+            label_IF100.Text = "IF100";
+            // 
+            // richTextBox_IF100
+            // 
+            richTextBox_IF100.Location = new Point(53, 233);
+            richTextBox_IF100.Name = "richTextBox_IF100";
+            richTextBox_IF100.Size = new Size(213, 386);
+            richTextBox_IF100.TabIndex = 7;
+            richTextBox_IF100.Text = "";
+            // 
+            // richTextBox_SPS101
+            // 
+            richTextBox_SPS101.Location = new Point(316, 233);
+            richTextBox_SPS101.Name = "richTextBox_SPS101";
+            richTextBox_SPS101.Size = new Size(213, 386);
+            richTextBox_SPS101.TabIndex = 9;
+            richTextBox_SPS101.Text = "";
+            // 
+            // label_SPS101
+            // 
+            label_SPS101.AutoSize = true;
+            label_SPS101.Location = new Point(389, 205);
+            label_SPS101.Name = "label_SPS101";
+            label_SPS101.Size = new Size(72, 25);
+            label_SPS101.TabIndex = 8;
+            label_SPS101.Text = "SPS101";
+            // 
+            // richTextBox_AllChannels
+            // 
+            richTextBox_AllChannels.Location = new Point(579, 233);
+            richTextBox_AllChannels.Name = "richTextBox_AllChannels";
+            richTextBox_AllChannels.Size = new Size(213, 386);
+            richTextBox_AllChannels.TabIndex = 11;
+            richTextBox_AllChannels.Text = "";
+            // 
+            // label_All
+            // 
+            label_All.AutoSize = true;
+            label_All.Location = new Point(672, 205);
+            label_All.Name = "label_All";
+            label_All.Size = new Size(32, 25);
+            label_All.TabIndex = 10;
+            label_All.Text = "All";
+            // 
+            // richTextBox_Actions
+            // 
+            richTextBox_Actions.Location = new Point(912, 108);
+            richTextBox_Actions.Name = "richTextBox_Actions";
+            richTextBox_Actions.Size = new Size(323, 511);
+            richTextBox_Actions.TabIndex = 13;
+            richTextBox_Actions.Text = "";
+            // 
+            // label_Actions
+            // 
+            label_Actions.AutoSize = true;
+            label_Actions.Location = new Point(1042, 80);
+            label_Actions.Name = "label_Actions";
+            label_Actions.Size = new Size(71, 25);
+            label_Actions.TabIndex = 12;
+            label_Actions.Text = "Actions";
+            // 
+            // label_ConnectedUsers
+            // 
+            label_ConnectedUsers.AutoSize = true;
+            label_ConnectedUsers.Location = new Point(355, 171);
+            label_ConnectedUsers.Name = "label_ConnectedUsers";
+            label_ConnectedUsers.Size = new Size(145, 25);
+            label_ConnectedUsers.TabIndex = 14;
+            label_ConnectedUsers.Text = "Connected Users";
+            // 
+            // Server_Application
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 704);
+            Controls.Add(label_ConnectedUsers);
+            Controls.Add(richTextBox_Actions);
+            Controls.Add(label_Actions);
+            Controls.Add(richTextBox_AllChannels);
+            Controls.Add(label_All);
+            Controls.Add(richTextBox_SPS101);
+            Controls.Add(label_SPS101);
+            Controls.Add(richTextBox_IF100);
+            Controls.Add(label_IF100);
+            Controls.Add(button_ServerStart);
+            Controls.Add(textBox_Port);
+            Controls.Add(label_Port);
+            Controls.Add(label_IP);
+            Controls.Add(textBox_IP);
+            Name = "Server_Application";
+            Text = "Server Application";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox_IP;
+        private Label label_IP;
+        private Label label_Port;
+        private TextBox textBox_Port;
+        private Button button_ServerStart;
+        private Label label_IF100;
+        private RichTextBox richTextBox_IF100;
+        private RichTextBox richTextBox_SPS101;
+        private Label label_SPS101;
+        private RichTextBox richTextBox_AllChannels;
+        private Label label_All;
+        private RichTextBox richTextBox_Actions;
+        private Label label_Actions;
+        private Label label_ConnectedUsers;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+namespace Server_Application_CS408
+{
+    public partial class Server_Application : Form
+    {
+
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private List<ClientInfo> clients = new List<ClientInfo>();
+
+        public Server_Application()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServer(string ip, int port)
+        {
+            try
+            {
+                IPAddress ipAddress;
+
+                if (IPAddress.TryParse(ip, out ipAddress))
+                {
+                    tcpListener = new TcpListener(ipAddress, port);
+                    listenThread = new Thread(new ThreadStart(ListenForClients));
+                    listenThread.Start();
+                    richTextBox_Actions.AppendText($"Server started on {ip}:{port}\n");
+                }
+                else
+                {
+                    richTextBox_Actions.AppendText($"Invalid IP address: {ip}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox_Actions.AppendText($"Error starting server: {ex.Message}\n");
+            }
+        }
+        private void UpdateRichTextBox(string message)
+        {
+            if (richTextBox_Actions.InvokeRequired)
+            {
+                richTextBox_Actions.Invoke(new Action<string>(UpdateRichTextBox), message);
+            }
+            else
+            {
+                richTextBox_Actions.AppendText(message);
+            }
+        }
+
+        private void ListenForClients()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                ClientInfo clientInfo = new ClientInfo(tcpClient);
+                clients.Add(clientInfo);
+
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(clientInfo);
+
+                UpdateRichTextBox($"Client connected: {clientInfo.IP}:{clientInfo.Port}\n");
+            }
+        }
+
+        private void HandleClientComm(object clientObj)
+        {
+            ClientInfo clientInfo = (ClientInfo)clientObj;
+            TcpClient tcpClient = clientInfo.TcpClient;
+            NetworkStream clientStream = tcpClient.GetStream();
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            try
+            {
+                while (true)
+                {
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                    if (bytesRead == 0)
+                        break;
+
+                    string data = Encoding.ASCII.GetString(message, 0, bytesRead);
+                    //UpdateRichTextBox($"Received message from {clientInfo.IP}:{clientInfo.Port}: {data}\n");
+                    ProcessMessage(clientInfo, data);
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateRichTextBox($"Error with client {clientInfo.IP}:{clientInfo.Port}: {ex.Message}\n");
+            }
+            finally
+            {
+                clientStream.Close();
+                tcpClient.Close();
+                clients.Remove(clientInfo);
+                UpdateRichTextBox($"Client disconnected: {clientInfo.IP}:{clientInfo.Port}\n");
+            }
+        }
+
+        private void ProcessMessage(ClientInfo clientInfo, string message)
+        {
+            // Assuming messages are formatted as "ACTION|CHANNEL|DATA"
+            string[] parts = message.Split('|');
+
+            if (parts.Length >= 2)
+            {
+                string action = parts[0].ToUpper();
+                string channel = parts[1].ToUpper();
+
+                switch (action)
+                {
+                    case "SUBSCRIBE":
+                        SubscribeToChannel(clientInfo, channel);
+                        break;
+                    case "UNSUBSCRIBE":
+                        UnsubscribeFromChannel(clientInfo, channel);
+                        break;
+                    case "SEND":
+                        if (parts.Length >= 3)
+                        {
+                            string data = parts[2];
+                            SendMessageToChannel(clientInfo, channel, data);
+                        }
+                        break;
+                        // Add more cases for other actions if needed
+                }
+            }
+            else
+            {
+                richTextBox_Actions.AppendText($"Invalid message format from {clientInfo.IP}:{clientInfo.Port}: {message}\n");
+            }
+        }
+
+        private List<ClientInfo> subscribedClientsIF100 = new List<ClientInfo>();
+        private List<ClientInfo> subscribedClientsSPS101 = new List<ClientInfo>();
+
+        private void SubscribeToChannel(ClientInfo clientInfo, string channel)
+        {
+            switch (channel.ToUpper())
+            {
+                case "IF100":
+                    if (!subscribedClientsIF100.Contains(clientInfo))
+                    {
+                        subscribedClientsIF100.Add(clientInfo);
+
+                        Invoke(new Action(() =>
+                        {
+                            richTextBox_IF100.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} subscribed to IF100\n");
+                        }));
+                        //richTextBox3.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} subscribed to IF100\n");
+                    }
+                    break;
+                case "SPS101":
+                    if (!subscribedClientsSPS101.Contains(clientInfo))
+                    {
+                        subscribedClientsSPS101.Add(clientInfo);
+
+                        Invoke(new Action(() =>
+                        {
+                            richTextBox_SPS101.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} subscribed to SPS101\n");
+                        }));
+                        //richTextBox4.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} subscribed to SPS101\n");
+                    }
+                    break;
+                default:
+
+
+                    Invoke(new Action(() =>
+                    {
+                        richTextBox_Actions.AppendText($"Invalid channel.");
+                    }));
+                    //richTextBox6.AppendText($"Invalid channel: {channel}\n");
+                    break;
+            }
+        }
+
+        private void UnsubscribeFromChannel(ClientInfo clientInfo, string channel)
+        {
+            switch (channel.ToUpper())
+            {
+                case "IF100":
+                    if (subscribedClientsIF100.Contains(clientInfo))
+                    {
+                        subscribedClientsIF100.Remove(clientInfo);
+
+                        richTextBox_IF100.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} unsubscribed from IF100\n");
+                    }
+                    break;
+                case "SPS101":
+                    if (subscribedClientsSPS101.Contains(clientInfo))
+                    {
+                        subscribedClientsSPS101.Remove(clientInfo);
+                        richTextBox_SPS101.AppendText($"Client {clientInfo.IP}:{clientInfo.Port} unsubscribed from SPS101\n");
+                    }
+                    break;
+                default:
+                    richTextBox_Actions.AppendText($"Invalid channel: {channel}\n");
+                    break;
+            }
+        }
+
+        private void SendMessageToChannel(ClientInfo sender, string channel, string data)
+        {
+            switch (channel.ToUpper())
+            {
+                case "IF100":
+                    foreach (var subscriber in subscribedClientsIF100)
+                    {
+                        if (subscriber != sender) // Don't send the message back to the sender
+                        {
+                            SendToClient(subscriber, $"Message from {sender.IP}:{sender.Port} on IF100: {data}");
+                        }
+                    }
+                    break;
+                case "SPS101":
+                    foreach (var subscriber in subscribedClientsSPS101)
+                    {
+                        if (subscriber != sender) // Don't send the message back to the sender
+                        {
+                            SendToClient(subscriber, $"Message from {sender.IP}:{sender.Port} on SPS101: {data}");
+                        }
+                    }
+                    break;
+                default:
+                    richTextBox_Actions.AppendText($"Invalid channel: {channel}\n");
+                    break;
+            }
+        }
+
+        private class ClientInfo
+        {
+            public TcpClient TcpClient { get; }
+            public string IP { get; }
+            public int Port { get; }
+
+            public ClientInfo(TcpClient tcpClient)
+            {
+                TcpClient = tcpClient;
+                var endPoint = (IPEndPoint)tcpClient.Client.RemoteEndPoint;
+                IP = endPoint.Address.ToString();
+                Port = endPoint.Port;
+            }
+        }
+
+        private void SendToClient(ClientInfo clientInfo, string message)
+        {
+            try
+            {
+                NetworkStream clientStream = clientInfo.TcpClient.GetStream();
+                byte[] buffer = Encoding.ASCII.GetBytes(message);
+                clientStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                richTextBox_Actions.AppendText($"Error sending message to {clientInfo.IP}:{clientInfo.Port}: {ex.Message}\n");
+            }
+        }
+
+        private void button_ServerStart_Click(object sender, EventArgs e)
+        {
+            string ip = textBox_IP.Text;
+            int port;
+
+            if (int.TryParse(textBox_Port.Text, out port))
+            {
+                StartServer(ip, port);
+            }
+            else
+            {
+                richTextBox_Actions.AppendText("Invalid port number.\n");
+                // Hasan Firat Yilmaz
+            }
+        }
+    }
+}
